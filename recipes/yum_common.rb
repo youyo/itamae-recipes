6341d3afb09c20082ec55f2c6a9a69d023bc792b
youyo <1003ni2@gmail.com>
2014-10-11 10:38:13 +0900

    define :rpm_package_from_url

--- recipes/yum_common.rb
+++ recipes/yum_common.rb
@@ -8,3 +8,12 @@ define :disable_repo do
     command "yum-config-manager --disable #{repo_name}"
   end
 end
+
+define :rpm_package_from_url, repo: nil do
+  url = params[:name]
+  repo_name = params[:repo]
+  package url do
+    only_if "test -e `yum-config-manager --showduplicates -q #{repo_name}|wc -l`"
+    action :install
+  end
+end
--- recipes/yum_remi.rb
+++ recipes/yum_remi.rb
@@ -1,5 +1,7 @@
 # yum_repo remi
 
 
-package 'http://rpms.famillecollet.com/enterprise/remi-release-6.rpm'
+rpm_package_from_url 'http://rpms.famillecollet.com/enterprise/remi-release-6.rpm' do
+  repo'remi'
+end
 disable_repo 'remi'
