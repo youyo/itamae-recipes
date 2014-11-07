fb037b340143c5f51f65d9bf4f59e4626cad19bf
youyo <1003ni2@gmail.com>
2014-11-07 22:13:21 +0900

    ntpdレシピ修正.

--- recipes/ntp.rb
+++ recipes/ntp.rb
@@ -3,11 +3,6 @@
 install_and_enable_package 'ntpdate'
 package 'ntp'
 
-execute 'restart ntpd' do
-  action :nothing
-  command 'sudo service ntpd restart >> /tmp/test.txt 2>&1'
-end
-
 execute 'ntpdate ntp.nict.jp' do
   not_if 'service ntpd status'
   command 'ntpdate -d ntp.nict.jp'
@@ -15,7 +10,7 @@ end
 
 template '/etc/ntp.conf' do
   source './templates/ntp.conf.erb'
-  notifies :run, "execute[restart ntpd]"
+  notifies :run, "execute[service ntpd restart]"
 end
 
 service 'ntpd' do
