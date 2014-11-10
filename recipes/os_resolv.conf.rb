77b711bb0e6c37456811006354c4a7fe5c29138d
youyo <1003ni2@gmail.com>
2014-11-10 22:46:20 +0900

    NM_CONTROLLED無効.

--- recipes/os_resolv.conf.rb
+++ recipes/os_resolv.conf.rb
@@ -1,3 +1,7 @@
+# OS resolv.conf
+
 template '/etc/resolv.conf' do
   source './templates/resolv.conf.erb'
 end
+
+execute 'sed -i "s|^NM_CONTROLLED|#NM_CONTROLLED|g" /etc/sysconfig/network-scripts/ifcfg-*'
