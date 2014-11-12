44d67405bb32a878b67698634a7c01816a084046
youyo <1003ni2@gmail.com>
2014-11-12 13:23:08 +0900

    パーミッション修正.

--- recipes/os_resolv.conf.rb
+++ recipes/os_resolv.conf.rb
@@ -9,5 +9,6 @@ node[:os][:resolv].each do |ip|
 end
 
 execute 'echo "options single-request-reopen" >> /etc/resolv.conf'
+execute 'chmod 644 /etc/resolv.conf'
 
 execute 'sed -i "s|^NM_CONTROLLED|#NM_CONTROLLED|g" /etc/sysconfig/network-scripts/ifcfg-*'
