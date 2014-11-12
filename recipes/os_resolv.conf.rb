de0a554a2f3c2f0612943d3452c077ccd4f41c66
youyo <1003ni2@gmail.com>
2014-11-12 22:47:00 +0900

    パーミッション修正.

--- recipes/os_resolv.conf.rb
+++ recipes/os_resolv.conf.rb
@@ -1,14 +1,10 @@
 # OS resolv.conf
 
-file '/etc/resolv.conf' do
-  action :delete
-end
+execute 'echo "options single-request-reopen" > /etc/resolv.conf'
+execute 'chmod 644 /etc/resolv.conf'
 
 node[:os][:resolv].each do |ip|
   execute "echo \"nameserver\t#{ip}\" >> /etc/resolv.conf"
 end
-
-execute 'echo "options single-request-reopen" >> /etc/resolv.conf'
-execute 'chmod 644 /etc/resolv.conf'
 
 execute 'sed -i "s|^NM_CONTROLLED|#NM_CONTROLLED|g" /etc/sysconfig/network-scripts/ifcfg-*'
