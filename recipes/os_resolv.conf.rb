1257c097b09801063fe89de949c58a3641c7066c
youyo <1003ni2@gmail.com>
2014-11-12 13:00:06 +0900

    resolv.conf周り修正.

--- recipes/os_resolv.conf.rb
+++ recipes/os_resolv.conf.rb
@@ -1,7 +1,13 @@
 # OS resolv.conf
 
-template '/etc/resolv.conf' do
-  source './templates/resolv.conf.erb'
+file '/etc/resolv.conf' do
+  action :delete
 end
+
+node[:os][:resolv].each do |ip|
+  execute "echo \"nameserver\t#{ip}\" >> /etc/resolv.conf"
+end
+
+execute 'echo "options single-request-reopen" >> /etc/resolv.conf'
 
 execute 'sed -i "s|^NM_CONTROLLED|#NM_CONTROLLED|g" /etc/sysconfig/network-scripts/ifcfg-*'
